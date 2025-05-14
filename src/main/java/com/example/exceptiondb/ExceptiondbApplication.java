d8e0fe4ca9e260cb651648ed649cc7aa326a655b
Skanda <skanda.td@gmail.com>
2025-05-14 12:29:00 +0530

    Spring init code

---
+++ src/main/java/com/example/exceptiondb/ExceptiondbApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptiondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptiondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptiondbApplication.class, args);
+	}
+
+}
